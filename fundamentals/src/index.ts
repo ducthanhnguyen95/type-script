f06ee012ea37c30c2e5790ec12a41bd5959002b6
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-06-13 13:06:52 +0700

    Built-in Types

---
+++ fundamentals/src/index.ts
@@ -0,0 +1,8 @@
+// let sales: number = 123_456_789;
+// let coures: string = 'TypeScript';
+// let is_published: boolean = true;
+
+let sales = 123_456_789;
+let coures = 'TypeScript';
+let is_published = true;
+let level;
