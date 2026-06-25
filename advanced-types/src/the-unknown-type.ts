85560ea51aa024fe132bbcdcd624b93b4eb7e352
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-06-25 17:31:45 +0700

    The unknown Type

---
+++ advanced-types/src/the-unknown-type.ts
@@ -0,0 +1,8 @@
+function render (document: unknown) {
+    // Narrowing
+    if(typeof document === 'string')
+        document.toUpperCase
+    document.move
+    document.fly
+    document.whateverWeWant
+}
