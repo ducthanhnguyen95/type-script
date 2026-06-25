99d6a65a9f03a06579e0c3b63a8ca948c4e0dabd
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-06-25 16:55:19 +0700

    Type Assertions

---
+++ advanced-types/src/type-assertions.ts
@@ -0,0 +1,7 @@
+let phone = document.getElementById('phone') as HTMLInputElement;
+let phone2 = <HTMLInputElement>document.getElementById('phone') as HTMLInputElement;
+
+// HTMLElement
+// HTMLInputElement
+phone.value
+phone2.value 
