1d7b1a42818f220ab4a38a1d1a56aa7c65920a31
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-04 11:38:56 +0700

    Getters and Setters

---
+++ classes-interfaces-and-object-oriented-programming/src/getters-and-setters.ts
@@ -0,0 +1,28 @@
+class Account {
+  nickname?: string;
+
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number,
+  ) {}
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    // Record a transaction
+    this._balance += amount;
+  }
+
+  get balance(): number {
+    return this._balance;
+  }
+
+  set balance(value: number) {
+    if (value < 0)  throw new Error("Invalid value");
+    this._balance = value;
+  }
+}
+
+let account = new Account(1, "Thanh", 0);
+console.log(account.balance);
+account.balance = 1;
