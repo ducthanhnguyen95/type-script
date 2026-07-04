ab2e76facd6b5308f92160830c4fdfb4711da891
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-04 10:01:09 +0700

    Access Control Keywords

---
+++ classes-interfaces-and-object-oriented-programming/src/access-control-keywords.ts
@@ -0,0 +1,27 @@
+class Account {
+  readonly id: number;
+  owner: string;
+  private _balance: number;
+  nickname?: string;
+
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this._balance = balance;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    // Record a transaction
+    this._balance += amount;
+  }
+
+  private calculaterTax() {}
+
+  getBalance(): number {
+    return this._balance;
+  }
+}
+
+let account = new Account(1, "Thanh", 0);
+console.log(account.getBalance());
