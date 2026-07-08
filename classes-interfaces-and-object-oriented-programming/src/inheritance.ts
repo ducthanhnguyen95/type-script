f9ad325330ab1c03e3b6d86cd18121ec33251833
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-08 16:52:47 +0700

    Inheritance

---
+++ classes-interfaces-and-object-oriented-programming/src/inheritance.ts
@@ -0,0 +1,30 @@
+class Person {
+  constructor(
+    public firstName: string,
+    public lastname: string,
+  ) {}
+
+  get fullName() {
+    return this.firstName + " " + this.lastname;
+  }
+
+  walk() {
+    console.log("Walking");
+  }
+}
+
+class Student extends Person {
+  constructor(
+    public studentId: number,
+    firstName: string,
+    lastName: string,
+  ) {
+    super(firstName, lastName);
+  }
+
+  takeTest() {
+    console.log("Taking a test");
+  }
+}
+
+let student = new Student(1, "Thanh", "Nguyen Duc");
