4cfb58f16adb96fe314f4faf0c9f9bbeb4c616e5
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-09 09:55:01 +0700

    Polymorphism

---
+++ classes-interfaces-and-object-oriented-programming/src/polymorphism.ts
@@ -0,0 +1,52 @@
+class Person {
+  constructor(
+    public firstName: string,
+    public lastname: string,
+  ) {}
+
+  get fullName() {
+    return this.firstName + " " + this.lastname;
+  }
+
+  walk() {
+    console.log("Walking");
+  }
+}
+
+class Student extends Person {
+  constructor(
+    public studentId: number,
+    firstName: string,
+    lastName: string,
+  ) {
+    super(firstName, lastName);
+  }
+
+  takeTest() {
+    console.log("Taking a test");
+  }
+}
+
+class Teacher extends Person {
+  override get fullName() {
+    return "Professor " + super.fullName;
+  }
+}
+
+class Principal extends Person {
+  override get fullName() {
+    return "Principal " + super.fullName;
+  }
+}
+
+printNames([
+  new Student(1, "Van", "Dai"),
+  new Teacher("Duc", "Thanh"),
+  new Principal("Marry", "Smith"),
+]);
+
+function printNames(people: Person[]) {
+  for (let person of people) {
+    console.log(person.fullName);
+  }
+}
