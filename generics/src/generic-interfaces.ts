bd7dd4359cbba5a58ce94aaada9001d6e9fc4aef
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-11 14:59:06 +0700

    Generic Interfaces

---
+++ generics/src/generic-interfaces.ts
@@ -0,0 +1,24 @@
+// http://mywebsite.com/users
+// http://mywebsite.com/products
+
+interface Result<T> {
+  data: T | null;
+  error: string | null;
+}
+
+function fetch<T>(url: string): Result<T> {
+  return { data: null, error: null };
+}
+
+interface User {
+  username: string;
+}
+
+interface Product {
+  title: string;
+}
+
+let result = fetch<User>("url");
+let result2 = fetch<Product>("url");
+result.data?.username;
+result2.data?.title;
