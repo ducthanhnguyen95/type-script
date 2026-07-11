709e411190cf8077ba79055e1c9236fdc91d51b5
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-11 12:59:48 +0700

    Generic Classes

---
+++ generics/src/generic-classes.ts
@@ -0,0 +1,8 @@
+class KeyValuePair<K, V> {
+  constructor(
+    public key: K,
+    public value: V,
+  ) {}
+}
+// let pair = new KeyValuePair<string, string>("1", "a");
+let pair = new KeyValuePair("1", "a");
