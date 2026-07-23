415ac21b011a5acc682ad9d288e89dd2e0326e77
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-23 15:50:47 +0700

    Parameterized Decorators

---
+++ decorators/src/parameterized-decorators.ts
@@ -0,0 +1,17 @@
+type ComponentOptions = {
+  selector: string;
+};
+
+function Component(options: ComponentOptions) {
+  return function Component(constructor: Function) {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now;
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM");
+    };
+  };
+}
+
+@Component({ selector: "#my-profile" })
+class ProfileComponent {}
