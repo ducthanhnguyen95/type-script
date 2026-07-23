ff2f2897b03ed7d3093fd7e45bcf500d88126cb9
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-07-23 18:01:02 +0700

    Method Decorators

---
+++ decorators/src/method-decorators.ts
@@ -0,0 +1,19 @@
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+
+  descriptor.value = function (...args: any) {
+    console.log("Before");
+    original.call(this, ...args);
+    console.log("After");
+  };
+}
+
+class Person {
+  @Log
+  say(message: string) {
+    console.log("Person say " + message);
+  }
+}
+
+let person = new Person();
+person.say("Hello");
