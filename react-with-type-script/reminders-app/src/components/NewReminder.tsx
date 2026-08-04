d0b652dfd3ed6088836f9d12da8fe4552e66e149
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-08-04 16:44:41 +0700

    Handling Form Submission

--- react-with-type-script/reminders-app/src/components/NewReminder.tsx
+++ react-with-type-script/reminders-app/src/components/NewReminder.tsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 
-function NewReminder(): React.JSX.Element {
+interface NewReminderProps {
+    onAddReminder: (title: string) => void
+}
+
+function NewReminder({ onAddReminder }: NewReminderProps): React.JSX.Element {
 
     const [title, setTitle] = useState('');
+    const submitForm = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!title) return
+        onAddReminder(title)
+        setTitle('')
+    }
 
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <label htmlFor=""></label>
             <input value={title} onChange={e => setTitle(e.target.value)} id="title" type="text" className="form-control" />
             <button type="submit" className="btn btn-primary my-3 rounded-pill">Add Reminder</button>
