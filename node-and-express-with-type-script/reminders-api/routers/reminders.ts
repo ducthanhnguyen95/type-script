c2a681c43478b1c4549e9034a1c29ba4b5ee7077
ducthanhnguyen95 <ducthanhnguyen95@gmail.com>
2026-08-05 16:25:48 +0700

    Building an API

--- node-and-express-with-type-script/reminders-api/routers/reminders.ts
+++ node-and-express-with-type-script/reminders-api/routers/reminders.ts
@@ -1,15 +1,59 @@
 import { Router } from "express";
 import CreateReminderDto from "../dtos/create-reminder";
+import Reminder from "../models/reminder";
 
 const router = Router();
+const reminders: Reminder[] = [];
 
 router.get("/", (req, res) => {
-  res.send("List of reminders");
+  res.json(reminders);
 });
 
 router.post("/", (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+  const reminder = new Reminder(title);
+  reminders.push(reminder);
+  res.status(201).json(reminder);
+});
+
+router.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (!Number.isFinite(id)) {
+    return res.status(400).json({ message: "Invalid reminder ID" });
+  }
+
+  const reminder = reminders.find((item) => item.id === id);
+
+  if (!reminder) {
+    return res.status(404).json({ message: "Reminder not found" });
+  }
+
+  const { title, isComplete } = req.body as Partial<
+    Pick<Reminder, "title" | "isComplete">
+  >;
+
+  if (title !== undefined) reminder.title = title;
+  if (isComplete !== undefined) reminder.isComplete = isComplete;
+
+  return res.json(reminder);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (!Number.isFinite(id)) {
+    return res.status(400).json({ message: "Invalid reminder ID" });
+  }
+
+  const reminderIndex = reminders.findIndex((item) => item.id === id);
+
+  if (reminderIndex === -1) {
+    return res.status(404).json({ message: "Reminder not found" });
+  }
+
+  reminders.splice(reminderIndex, 1);
+  return res.status(204).send();
 });
 
 export default router;
